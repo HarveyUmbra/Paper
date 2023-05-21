e46529d926566fa6e5d45644d812211b7df445f7
HarveyUmbra <105540110+HarveyUmbra@users.noreply.github.com>
2023-05-21 11:52:26 +0200

    sdasd
    Anderungen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, winit::WinitSettings};
+
 fn main() {
-    App::new().run()
-    
-    println!("Hello, world!");
+   App::new()
+    .add_plugins(DefaultPlugins)
+    .insert_resource(WinitSettings::desktop_app())
+    .add_startup_system(setup)
+    .run();
 }
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+//Alice, Bob, Carol, Dave, Eve, Mallory, Oscar, and Trent 
